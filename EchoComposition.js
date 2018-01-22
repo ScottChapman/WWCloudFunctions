a6d573bbd08d0b82a6196d1bfe4bd0d5a3e6cae3
Scott Chapman <scottchapman@us.ibm.com>
2018-01-22 15:17:32 -0500

    Added composer app

---
+++ EchoComposition.js
@@ -0,0 +1,15 @@
+composer.if(params =>
+  {
+    return !params.myEvent &&
+      params.hasOwnProperty("content") &&
+      params.content.toLowerCase().startsWith("can you echo") &&
+      params.hasOwnProperty("spaceId")
+  },
+	composer.sequence(params => {
+		return {
+			spaceId: params.spaceId,
+			title: "From Composer Bot",
+			text: "Of course I can!"
+		};
+	},"WatsonWork/WWSendMessage")
+);
